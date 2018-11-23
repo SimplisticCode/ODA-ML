2c66bd7b8cb0d98dd9606a8a5439606b832f56bf
Simon Thrane Hansen <sthranehansen@gmail.com>
2018-11-23 17:43:34 +0100

    Latex plots
    Added plots

--- mnist.py
+++ mnist.py
@@ -1,11 +1,15 @@
+from matplotlib.colors import ListedColormap
 from sklearn.decomposition import PCA
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 from MNIST.loadImages import loadMNISTImages, loadMNISTLabels
 from NearestCentroid import NearestCentroid
 from NearestSubclassCentroid import NearestSubclassCentroid
-from kNearestNeighbor import kNearestNeighbor
+from Perceptron_multiclass_backpropagation import get_classifier
+from Perceptron_multiclass_lms import Perceptron_multiclass_lms
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 
@@ -21,12 +25,6 @@ y_test = loadMNISTLabels(filenameTest['labels'])
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
 
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
 scaler = StandardScaler()
 
 # Standardizing the features
@@ -41,61 +39,137 @@ pca = PCA(n_components=2)
 x_train_pca = pca.fit_transform(x_train)
 x_test_pca = pca.fit_transform(x_test)
 
-#KNearestNeighbor
-# making our predictions
-predictions = []
+# Construct the colormap
+current_palette = sns.color_palette("muted", n_colors=len(set(y_train)))
+cmap = ListedColormap(sns.color_palette(current_palette).as_hex())
 
-# On original data
-#kNearestNeighbor(x_train[:5000], y_train[:5000], x_test[:1000], predictions, 1)
+# Create a scatter plot of the data
+plt.scatter(x_test_pca[:, 0], x_test_pca[:, 1], c=y_test, cmap=cmap)
+plt.title("Scatterplot mnist test")
 
-# On 2d data
-#kNearestNeighbor(x_train_pca[:5000], y_train[:5000], x_test_pca[:1000], predictions, 1)
+# Add a color bar
+plt.colorbar()
 
-# transform the list into an array
-predictions = np.asarray(predictions)
+# Show the plot
+plt.show()
+plt.savefig("scatter_plot_mnist.png", bbox_inches='tight')
 
+# Accuracy data:
+accuracy_table_data = []
+
+# KNearestNeighbor
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train, y_train)
+predictions = knn.predict(x_test)
 # evaluating accuracy
-#accuracy = accuracy_score(y_test[:1000], predictions)
-#print('\nThe accuracy of our classifier is %d%%' % accuracy * 100)
+accuracy_knn = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["KNearestNeighbor", accuracy_knn])
 
-# train using K-NN
+# Perceptron Multiclass Least Mean Square
+perc_lms = Perceptron_multiclass_lms();
+perc_lms.train(x_train, y_train)
+predictions = perc_lms.predict(x_test)
+accuracy_lms = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["Perceptron Multiclass Least Mean Square", accuracy_lms])
 
-#Nearest Centroid
-#Normal data:
+# Perceptron Multiclass Backpropagation:
+Perceptron_backpropagation = get_classifier();
+Perceptron_backpropagation.fit(x_train, y_train)
+predictions = Perceptron_backpropagation.predict(x_test)
+accuracy_backpropagation = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["Perceptron Backpropagation", accuracy_backpropagation])
+
+# NearestCentroid
+# Normal data
 ncc = NearestCentroid()
 ncc.fit(x_train, y_train)
 # get the model accuracy
 predictions = ncc.predict(x_test)
-predictions = np.asarray(predictions)
-
-accuracy = accuracy_score(y_test, predictions)
-print('The accuracy of our NearestCentroid classifier is ' + str(accuracy * 100))
-
-
-#Visualize data:
-plt.scatter(x_test_pca[:,0], x_test_pca[:,1], c=y_test, label=y_test)
-plt.legend()
-plt.show()
-
+accuracy_ncc = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["NearestCentroid", accuracy_ncc])
 
 # Nearest subclass classifier (2 subclasses):
 nsc = NearestSubclassCentroid()
 nsc.fit(x_train, y_train, 2)
 predictions = nsc.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
-print('The accuracy of our NearestSubclassCentroid (with 2 subclasses) classifier is ' + str(accuracy * 100))
+accuracy_nsc2 = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["Nearest subclass classifier 2", accuracy_nsc2])
 
 # Nearest subclass classifier (3 subclasses):
 nsc3 = NearestSubclassCentroid()
 nsc3.fit(x_train, y_train, 3)
 predictions = nsc3.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
-print('The accuracy of our NearestSubclassCentroid (with 3 subclasses) classifier is ' + str(accuracy * 100))
+accuracy_nsc3 = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["Nearest subclass classifier 3", accuracy_nsc3])
 
 # Nearest subclass classifier (5 subclasses):
 nsc5 = NearestSubclassCentroid()
 nsc5.fit(x_train, y_train, 5)
 predictions = nsc5.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
-print('The accuracy of our NearestSubclassCentroid (with 5 subclasses) classifier is ' + str(accuracy * 100))
+accuracy_nsc5 = accuracy_score(y_test, predictions)
+accuracy_table_data.append(["Nearest subclass classifier 5", accuracy_nsc5])
+
+
+#2 Data
+accuracy2d = []
+# KNearestNeighbor using sklearn own implementation is too slow on this data set
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train_pca, y_train)
+predictions = knn.predict(x_test_pca)
+# evaluating accuracy
+accuracy_knn = accuracy_score(y_test, predictions)
+accuracy2d.append(["KNearestNeighbor", accuracy_knn])
+
+# Perceptron Multiclass Least Mean Square
+perc_lms = Perceptron_multiclass_lms();
+perc_lms.train(x_train_pca, y_train)
+predictions = perc_lms.predict(x_test_pca)
+accuracy_lms = accuracy_score(y_test, predictions)
+accuracy2d.append(["Perceptron Multiclass Least Mean Square", accuracy_lms])
+
+# Perceptron Multiclass Backpropagation:
+Perceptron_backpropagation = get_classifier();
+Perceptron_backpropagation.fit(x_train_pca, y_train)
+predictions = Perceptron_backpropagation.predict(x_test_pca)
+accuracy_backpropagation = accuracy_score(y_test, predictions)
+accuracy2d.append(["Perceptron Backpropagation", accuracy_backpropagation])
+
+# NearestCentroid
+# Normal data
+ncc = NearestCentroid()
+ncc.fit(x_train_pca, y_train)
+# get the model accuracy
+predictions = ncc.predict(x_test_pca)
+accuracy_ncc = accuracy_score(y_test, predictions)
+accuracy2d.append(["NearestCentroid", accuracy_ncc])
+
+# Nearest subclass classifier (2 subclasses):
+nsc = NearestSubclassCentroid()
+nsc.fit(x_train_pca, y_train, 2)
+predictions = nsc.predict(x_test_pca)
+accuracy_nsc2 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 2", accuracy_nsc2])
+
+# Nearest subclass classifier (3 subclasses):
+nsc3 = NearestSubclassCentroid()
+nsc3.fit(x_train_pca, y_train, 3)
+predictions = nsc3.predict(x_test_pca)
+accuracy_nsc3 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 3", accuracy_nsc3])
+
+# Nearest subclass classifier (5 subclasses):
+nsc5 = NearestSubclassCentroid()
+nsc5.fit(x_train, y_train, 5)
+predictions = nsc5.predict(x_test)
+accuracy_nsc5 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 5", accuracy_nsc5])
+
+df = pd.DataFrame()
+df['Algortihms'] = [e[0] for e in accuracy_table_data]
+df['Accuracy full data set'] = [e[1] for e in accuracy_table_data]
+df['Accuracy pca data (2D)'] = [e[1] for e in accuracy2d]
+with open('Minst.tex','w') as tf:
+    tf.write(df.to_latex())
+
+print(df.all)
 
--- orlTraining.py
+++ orlTraining.py
@@ -1,8 +1,3 @@
-import subprocess
-import types
-
-import numpy as np
-import six
 from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.preprocessing import StandardScaler
@@ -110,42 +105,65 @@ predictions = nsc5.predict(x_test)
 accuracy_nsc5 = accuracy_score(y_test, predictions)
 accuracy_table_data.append(["Nearest subclass classifier 5", accuracy_nsc5])
 
+
+#2 Data
+accuracy2d = []
+predictions = []
+# KNearestNeighbor
+kNearestNeighbor(x_train_pca, y_train, x_test_pca, predictions, 3)
+# evaluating accuracy
+accuracy_knn = accuracy_score(y_test, predictions)
+accuracy2d.append(["KNearestNeighbor", accuracy_knn])
+
+# Perceptron Multiclass Least Mean Square
+perc_lms = Perceptron_multiclass_lms();
+perc_lms.train(x_train_pca, y_train)
+predictions = perc_lms.predict(x_test_pca)
+accuracy_lms = accuracy_score(y_test, predictions)
+accuracy2d.append(["Perceptron Multiclass Least Mean Square", accuracy_lms])
+
+# Perceptron Multiclass Backpropagation:
+Perceptron_backpropagation = get_classifier();
+Perceptron_backpropagation.fit(x_train_pca, y_train)
+predictions = Perceptron_backpropagation.predict(x_test_pca)
+accuracy_backpropagation = accuracy_score(y_test, predictions)
+accuracy2d.append(["Perceptron Backpropagation", accuracy_backpropagation])
+
+# NearestCentroid
+# Normal data
+ncc = NearestCentroid()
+ncc.fit(x_train_pca, y_train)
+# get the model accuracy
+predictions = ncc.predict(x_test_pca)
+accuracy_ncc = accuracy_score(y_test, predictions)
+accuracy2d.append(["NearestCentroid", accuracy_ncc])
+
+# Nearest subclass classifier (2 subclasses):
+nsc = NearestSubclassCentroid()
+nsc.fit(x_train_pca, y_train, 2)
+predictions = nsc.predict(x_test_pca)
+accuracy_nsc2 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 2", accuracy_nsc2])
+
+# Nearest subclass classifier (3 subclasses):
+nsc3 = NearestSubclassCentroid()
+nsc3.fit(x_train_pca, y_train, 3)
+predictions = nsc3.predict(x_test_pca)
+accuracy_nsc3 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 3", accuracy_nsc3])
+
+# Nearest subclass classifier (5 subclasses):
+nsc5 = NearestSubclassCentroid()
+nsc5.fit(x_train, y_train, 5)
+predictions = nsc5.predict(x_test)
+accuracy_nsc5 = accuracy_score(y_test, predictions)
+accuracy2d.append(["Nearest subclass classifier 5", accuracy_nsc5])
+
 df = pd.DataFrame()
 df['Algortihms'] = [e[0] for e in accuracy_table_data]
-df['Accuracy'] = [e[1] for e in accuracy_table_data]
+df['Accuracy full data set'] = [e[1] for e in accuracy_table_data]
+df['Accuracy pca data (2D)'] = [e[1] for e in accuracy2d]
+with open('Orltable.tex','w') as tf:
+    tf.write(df.to_latex())
+
 print(df.all)
-
-# 2d data
-ncc1 = NearestCentroid()
-ncc1.fit(x_train_pca, y_train)
-# get the model accuracy
-predictions = ncc1.predict(x_test_pca)
-accuracy = accuracy_score(y_test, predictions)
-
-
-def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
-                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
-                     bbox=[0, 0, 1, 1], header_columns=0,
-                     ax=None, **kwargs):
-    if ax is None:
-        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
-        fig, ax = plt.subplots(figsize=size)
-        ax.axis('off')
-
-    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
-
-    mpl_table.auto_set_font_size(False)
-    mpl_table.set_fontsize(font_size)
-
-    for k, cell in six.iteritems(mpl_table._cells):
-        cell.set_edgecolor(edge_color)
-        if k[0] == 0 or k[1] < header_columns:
-            cell.set_text_props(weight='bold', color='w')
-            cell.set_facecolor(header_color)
-        else:
-            cell.set_facecolor(row_colors[k[0] % len(row_colors)])
-    return ax
-
-# found https://stackoverflow.com/questions/26678467/export-a-pandas-dataframe-as-a-table-image/26681726
-
-render_mpl_table(df, header_columns=0, col_width=2.0)
